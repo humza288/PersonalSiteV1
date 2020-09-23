a279392eff574f2b0294e6a86a97ce973daceb08
Humza Munir <humza288@gmail.com>
2020-09-23 02:38:00 -0700

    fixed mobile bug

---
+++ app.js
@@ -0,0 +1,9 @@
+var app = angular.module("portfolioSite", []); 
+
+app.controller("mainCtrl", function($scope) {
+    $scope.submitContact = function () {
+        console.log("chod man")
+    }
+});
+
+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
--- scripts/animateCards.js
+++ scripts/animateCards.js
@@ -1,20 +1,18 @@
 
 // code for animating the cards
 
-
 const boxes = document.querySelectorAll('.content');
       
 let observer = new IntersectionObserver((enteries) => {
   enteries.forEach(entry => {
+    if (screen && screen.width < 1030) {
       if(entry.intersectionRatio > 0 ) {
-        entry.target.style.transform = 'translate(0)';
-        console.log("hi")
-  
+        entry.target.style.transform = 'translate(0)';  
       }
       else {
         entry.target.style.transform = 'translate(-100%)';
-        console.log("hi")
       }
+    } 
   })
 })
 
